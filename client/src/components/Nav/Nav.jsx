b3bc1b62c31174ee94b9d41e25357c9caa666d5b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-14 09:46:00 +0530

    Update

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -4,8 +4,10 @@ const Nav = () => {
   return (
     <div className='py-6 xl:px-24 md:px-10 px-4 bg-sky-600/80 border-b border-gray-200 shadow-xl text-white'>
         <div className="flex justify-between">
-            <div className="">asdasd</div>
-            <div className="">asdasd</div>
+            <div className="font-semibold uppercase">Buddyfy Connect</div>
+            <div className="">
+                <img src="https://avatars.githubusercontent.com/u/138636749?v=4" alt="" className='h-8 rounded-full w-auto'/>
+            </div>
         </div>
     </div>
   )
--- client/src/page/Posts/Posts.jsx
+++ client/src/page/Posts/Posts.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import DefaultInput from '../../components/Forms/DefaultInput'
-import DefaultTextArea from '../../components/Forms/DefaultTextArea'
-import DefualtSelect from '../../components/Forms/DefualtSelect'
 
 const Posts = () => {
-    const inputoption = [
-        {name: 'Option1', value: 'Option 1'},
-        {name: 'Option2', value: 'Option 2'},
-        {name: 'Option3', value: 'Option 3'},
-        {name: 'Option4', value: 'Option 4'},
-        {name: 'Option5', value: 'Option 5'},
-    ]
   return (
     <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, magni eveniet quo eaque nisi nihil sit sequi inventore itaque dolorem, illum culpa dignissimos doloremque ea perspiciatis laborum fugiat error? Reprehenderit.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente at ipsam sequi obcaecati molestias consectetur, rerum labore mollitia voluptatibus fugiat a, consequuntur, pariatur veniam accusantium iure? Hic eos consequuntur odit?
     </div>
   )
 }
