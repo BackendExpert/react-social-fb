a5e7a18361da9bce3b82eeb0c7ad43a9ae014f7e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-14 17:27:34 +0530

    Update Nav.jsx

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <div className='py-6 xl:px-24 md:px-10 px-4 bg-sky-600/80 border-b border-gray-200 shadow-xl text-white'>
         <div className="flex justify-between">
             <div className="font-semibold uppercase">Buddyfy Connect</div>
-            <div className="">
+            <div className="cursor-pointer">
                 <img src="https://avatars.githubusercontent.com/u/138636749?v=4" alt="" className='h-8 rounded-full w-auto'/>
             </div>
         </div>
