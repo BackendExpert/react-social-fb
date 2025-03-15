7bec71fce88c7cd6750187896f9d22447d9c48c6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-15 12:02:48 +0530

    Update

--- client/src/page/AboutMe/UpdateAboutMe.jsx
+++ client/src/page/AboutMe/UpdateAboutMe.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DefaultInput from '../../components/Forms/DefaultInput';
 
 const UpdateAboutMe = () => {
+    const [updatedata, setupdatedata] = useState({
+        name: '',
+        works: '',
+        studies: '',
+        live: '',
+        from: '',
+        status: '',
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setupdatedata((prevData) => ({
+          ...prevData,
+          [name]: value
+        }));
+    };
+
+    const headleUpdateData = (e) => {
+        e.preventDefault()
+        try{
+            console.log(updatedata)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
   return (
     <div className='bg-white p-4 rounded-lg shadow-md'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, sed quibusdam! Non atque ad dolorem corrupti, laudantium reprehenderit, natus odit debitis aliquam, dignissimos explicabo tempore repellendus culpa id. Maiores, nobis.
+        <form onSubmit={headleUpdateData} method="post">
+            <div className="">
+                <DefaultInput 
+                    type={'text'}
+                    name={'name'}
+                    value={updatedata.name}
+                    placeholder={"Name"}
+                    onChange={handleInputChange}
+                />
+            </div>
+        </form>
     </div>
   )
 }
--- client/src/page/AboutMe/ViewAboutMe.jsx
+++ client/src/page/AboutMe/ViewAboutMe.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 const ViewAboutMe = () => {
   return (
     <div className='bg-white p-4 rounded-lg shadow-md'>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus eligendi commodi labore quas delectus quae asperiores velit dolorem dignissimos. Rem excepturi libero labore nulla voluptatibus? Dolorum consequuntur natus aliquam perferendis.
+        <table className='w-full'>
+            <tr className='h-12'>
+                <td className='text-gray-500 font-semibold'>Name: </td>
+                <td>Jehan Weerasuriya</td>
+            </tr>
+            <tr className='h-12'>
+                <td className='text-gray-500 font-semibold'>Works at: </td>
+                <td>JKCSS Framework</td>
+            </tr>
+            <tr className='h-12'>
+                <td className='text-gray-500 font-semibold'>Studied at: </td>
+                <td>ABC College</td>
+            </tr>
+            <tr className='h-12'>
+                <td className='text-gray-500 font-semibold'>Lives in: </td>
+                <td>Kandy</td>
+            </tr>
+            <tr className='h-12'>
+                <td className='text-gray-500 font-semibold'>From: </td>
+                <td>Colombo</td>
+            </tr>
+            <tr className='h-12'>
+                <td className='text-gray-500 font-semibold'>Status: </td>
+                <td>Single</td>
+            </tr>
+        </table>
     </div>
   )
 }
