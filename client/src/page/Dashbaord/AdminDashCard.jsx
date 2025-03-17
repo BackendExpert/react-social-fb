643718f89ceedaf36565e502d7acd54883e9fa3f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-17 07:59:35 +0530

    Create AdminDashCard.jsx

---
+++ client/src/page/Dashbaord/AdminDashCard.jsx
@@ -0,0 +1,30 @@
+import { FaUsers } from "react-icons/fa6";
+import { MdReport, MdToday, MdAdminPanelSettings } from "react-icons/md";
+
+
+const admindata = [
+    {
+        id: 1,
+        name: 'Users',
+        icon: FaUsers,
+        value: '',
+    },
+    {
+        id: 2,
+        name: 'Reported Users',
+        icon: MdReport,
+        value: '',
+    },
+    {
+        id: 3,
+        name: 'Today Joined',
+        icon: MdToday,
+        value: '',
+    },
+    {
+        id: 4,
+        name: 'Total Admins',
+        icon: MdAdminPanelSettings,
+        value: '',
+    },
+]
