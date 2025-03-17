e4deb6b9728073c32f147b39a6d57f6c1e40a72c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-17 07:22:23 +0530

    Update

--- client/src/page/BirthDays/AllBirthDays.jsx
+++ client/src/page/BirthDays/AllBirthDays.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const AllBirthDays = () => {
   return (
     <div className='bg-white p-4 rounded-md shadow-md'>
-        AllBirthDays
+        <h1 className="text-gray-500 font-semibold">BirthDays</h1>
+
+        <div className="">
+            <div className="grid grid-cols-3 gap-3 mt-4">
+                <div className="flex">
+                    <img src="https://avatars.githubusercontent.com/u/138636749?v=4" alt="" className='h-20 rounded-full w-auto'/>
+                    <div className="pl-4 pt-3">
+                        <h1 className="text-xl text-gray-500 font-semibold">Jehan Weerasuriya</h1>
+                        <p className="">25 years old</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
--- client/src/page/BirthDays/UpcomingBD.jsx
+++ client/src/page/BirthDays/UpcomingBD.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 const UpcomingBD = () => {
-  return (
-    <div className='bg-white p-4 rounded-md shadow-md'>
-        UpcomingBD
-    </div>
-  )
+    return (
+        <div className='bg-white p-4 rounded-md shadow-md'>
+            <h1 className="text-gray-500 font-semibold">Up Coming BirthDays</h1>
+            <div className="grid grid-cols-3 gap-3 mt-4">
+                <div className="flex">
+                    <img src="https://avatars.githubusercontent.com/u/138636749?v=4" alt="" className='h-20 rounded-full w-auto' />
+                    <div className="pl-4 pt-3">
+                        <h1 className="text-xl text-gray-500 font-semibold">Jehan Weerasuriya</h1>
+                        <p className="">after 2 days</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default UpcomingBD
