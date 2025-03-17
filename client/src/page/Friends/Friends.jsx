b5873eb5a6a18e66b4bee09a832091cd14f795c5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-17 07:12:09 +0530

    Update

--- client/src/page/Friends/Friends.jsx
+++ client/src/page/Friends/Friends.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Friends = () => {
-  return (
-    <div>Friends</div>
-  )
+    return (
+        <div>
+            <div className="grid xl:grid-cols-3 md:grid-cols-2 gap-4">
+                <div className="bg-white p-4 shadow-lg">
+                    <div className="flex">
+                        <img src="https://avatars.githubusercontent.com/u/138636749?v=4" alt="" className='h-16 w-auto rounded-full' />
+                        <a href="#"><h1 className="text-xl font-semibold pl-4 mt-2">Jehan Weerasuriya</h1></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Friends
--- client/src/page/Photos/Photos.jsx
+++ client/src/page/Photos/Photos.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Photos = () => {
   return (
-    <div>Photos</div>
+    <div>
+        <div className="grid grid-cols-5 gap-3">
+            <div className="">
+                <img src="https://wallpapercave.com/wp/wp2707503.jpg" alt="" className='h-40 w-auto'/>
+            </div>
+        </div>
+    </div>
   )
 }
 
