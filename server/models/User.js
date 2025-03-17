3eea706a660fd1c38c8f890debf06dbfacb4bbf4
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-17 08:16:13 +0530

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, minlength: 6 },
+    isAdmin: { type: Boolean, default: false }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
