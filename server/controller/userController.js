d501aa582f7d3c8d376f41e975666f3624dd5723
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-20 06:11:32 +0530

    Update userController.js

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,33 @@
-const userController = {
+const UserData = require('../models/UserData');
+const UserActivity = require('../models/UserActivity')
 
+const userController = {
+    updateCoverImage: async(req, res) => {
+        try{
+            const email = req.params.email;
+            const coverImg = req.file.path;
+
+            const CoverImgUpdate = await UserData.findOneAndUpdate(
+                { email: email },
+                { $set: { coverimg: coverImg } }, 
+                { new: true } 
+            );
+
+            if(CoverImgUpdate){
+                const newactivity = new UserActivity({
+                    email: email,
+                    activity: "Update Cover Image"
+                })
+
+                if(newactivity){
+                    return res.json({ Status: "Success"})
+                }
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 };
 
 module.exports = userController;
