f84c89fd079fc0522e7050fc1fac1613ba09a9ca
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-20 06:21:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 // routes
 const ConnectDB = require('./config/DB')
 const authRoute = require('./routes/authRoute')
+const userRoute = require('./routes/userRoute')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -23,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/auth', authRoute)
+app.use('/user', userRoute)
 
 
 app.get('/', (req, res) => {
